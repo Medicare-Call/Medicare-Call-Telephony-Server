5f20c377d2b00464c1189c52d07b929fb63fe716
kisusu <kikisu115@naver.com>
2026-02-09 00:00:58 +0900

    refactor: STT 모듈 파일명 벤더 기반 네이밍 적용 및 주석 정리

--- websocket-server/src/services/tts/index.ts
+++ websocket-server/src/services/tts/index.ts
@@ -3,7 +3,6 @@ import { defaultConfig } from './elevenlabs.config';
 
 export const ttsService = new ElevenLabsService(defaultConfig);
 
-// 타입 및 클래스 re-export
 export { ElevenLabsService } from './elevenlabs.service';
 export type { ElevenLabsConfig } from './elevenlabs.config';
 export type { ElevenLabsSession, ElevenLabsStreamResult } from './elevenlabs.types';
